4bcc4ff4c8ec54c466b0110a95c7360d920bb12c
João Victor <soulima.joao@gmail.com>
2021-11-25 13:26:48 -0300

    estruturação da tabela com EJS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Processor = require('./Processor')
 const Reader = require('./Reader')
 const Table = require('./Table')
+const HTMLParser = require('./HTMLParser')
 
 const leitor = new Reader()
 
@@ -8,9 +9,9 @@ async function main() {
     const data = await leitor.read('./users.csv')
     const processed_data = Processor.process(data)
     const usuarios = new Table(processed_data)
-    console.log(usuarios)
-    console.log(usuarios.rowCount)
-    console.log(usuarios.columnCount)
+    
+    const html = await HTMLParser.parse(usuarios)
+    console.log(html)
 
 }
 
